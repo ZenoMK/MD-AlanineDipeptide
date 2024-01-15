993c14bc57f60b6de0e5fff8fa2066fd90987907
Zeno Kujawa <30478937+ZenoMK@users.noreply.github.com>
2024-01-15 09:45:15 +0000

    Update main_2d.py

--- GP/main_2d.py
+++ GP/main_2d.py
@@ -10,7 +10,7 @@ from sklearn.metrics import mean_squared_error
 
 
 
-def main(kernel_author="vlad", kernel_number=None):
+def main(kernel_author=None, kernel_number=None):
     """
     kernel_author: add your name in match statement below
     kernel_number: add numbered kernels under your name as you experiment w different kernels
@@ -21,8 +21,12 @@ def main(kernel_author="vlad", kernel_number=None):
     mean_hist, std_hist, temps, concs, histograms = load_histograms_and_calculate_stats(hist_data_dir)
 
     X, Y = prepare_2d_gp_data(histograms, temps, concs)
+    print(X.shape)
+    print(Y.shape)
     X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.10, random_state=42)
     scaler = StandardScaler()
+    #X_train = X
+    #Y_train = Y
     # scale train data
     X_train_scaled = scaler.fit_transform(X_train)
     # scale test data
@@ -50,16 +54,16 @@ def main(kernel_author="vlad", kernel_number=None):
 
     # Create and optimize GP model
     m = GPy.models.GPRegression(X_train_scaled, Y_train, kernel)
-    m.optimize(messages=True, max_iters = 1)
+    m.optimize(messages=True)
     #m.pickle(f'../output/models/{kernel_author}_{kernel_number}_model_save')
     np.save(f'../output/models/{kernel_author}_{kernel_number}_model_save.npy', m.param_array)
 
     # Predict on the test set and calculate MSE
     Y_pred, _ = m.predict(X_test_scaled)
     mse = mean_squared_error(Y_test, Y_pred)
-    print(Y_pred)
-    #np.save(f'../output/predictions/{kernel_author}_{kernel_number}_model_save.npy', Y_pred)
-    #print(f"MSE: {mse}")
+    #print(Y_pred.shape)
+    np.save(f'../output/predictions/{kernel_author}_{kernel_number}_model_save.npy', Y_pred)
+    print(f"MSE: {mse}")
     return m, mse
 
 
@@ -70,4 +74,4 @@ if __name__ == '__main__':
     parser.add_argument('--kernel_number', type=int)
     args = parser.parse_args()
 
-    main(kernel_author=args.kernel_author, kernel_number=args.kernel_number)
+    main(kernel_author=args.kernel_author, kernel_number = args.kernel_number)

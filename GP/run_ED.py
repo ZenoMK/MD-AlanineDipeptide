9985bab18ba08fc944b9e85ddb164061ea24ab3e
ZenoMK <zeno.kujawa@gmail.com>
2024-01-16 20:28:58 +0000

    fun plotting part 2: electric bugaloo

--- GP/run_ED.py
+++ GP/run_ED.py
@@ -7,7 +7,7 @@ from utils_angles_fixed import *
 from sklearn.metrics import mean_absolute_error, mean_squared_error
 import matplotlib.pyplot as plt
 
-logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.WARN)
 
 dir = hist_data_dir = f"{os.path.dirname(os.path.dirname(os.path.realpath(__file__)))}/data_processing/data_processed"
 
@@ -15,9 +15,11 @@ test_hists = f"{os.path.dirname(os.path.dirname(os.path.realpath(__file__)))}/da
 mean_histogram, std_histogram, temps, concs, histograms = load_histograms_and_calculate_stats(test_hists)
 X_test, Y_test = prepare_2d_gp_data(histograms, temps, concs)
 
-RMSE = np.zeros((10,30))
-MAE = np.zeros((10,30))
-for i in range(10):
+RMSE = np.empty((5,30))
+RMSE[:] = np.nan
+MAE = np.empty((5,30))
+MAE[:] = np.nan
+for i in range(5):
     obj = ExperimentalDesign(dir)
     m = obj.get_model()
     Y_pred, _ = m.predict(X_test)
@@ -40,26 +42,28 @@ for i in range(10):
         RMSE[i,j] = rmse
         MAE[i,j] = mae
 
-RMSE_means = np.mean(RMSE, axis = 1)
-MAE_means = np.mean(MAE, axis = 1)
+    RMSE_means = np.nanmean(RMSE, axis = 1)
+    MAE_means = np.nanmean(MAE, axis = 1)
+    np.save(RMSE, "../results/output/RMSE.npy")
+    np.save(RMSE, "../results/output/MAE.npy")
 
-print(RMSE_means)
-print(MAE_means)
+    print(RMSE_means)
+    print(MAE_means)
 
-RMSE_std = np.std(RMSE, axis = 1)
-MAE_std = np.std(MAE, axis = 1)
-idx = [i for i in range(10)]
-#fig, ax = plt.figure()
-plt.plot(idx, RMSE_means, color = "blue", label = "RMSE")
-plt.fill_between(x = idx, y1 = RMSE_means-RMSE_std, y2 = RMSE_means+RMSE_std, color = "blue", alpha = 0.1)
+    RMSE_std = np.nanstd(RMSE, axis = 1)
+    MAE_std = np.nanstd(MAE, axis = 1)
+    idx = [i for i in range(30)]
+    #fig, ax = plt.figure()
+    plt.plot(idx, RMSE_means, color = "blue", label = "RMSE")
+    plt.fill_between(x = idx, y1 = RMSE_means-RMSE_std, y2 = RMSE_means+RMSE_std, color = "blue", alpha = 0.1)
 
-plt.plot(idx, MAE_means, color = "red", label = "MAE")
-plt.fill_between(x = idx, y1 = MAE_means-MAE_std,y2 = RMSE_means+RMSE_std, color = "red", alpha = 0.1)
+    plt.plot(idx, MAE_means, color = "red", label = "MAE")
+    plt.fill_between(x = idx, y1 = MAE_means-MAE_std,y2 = RMSE_means+RMSE_std, color = "red", alpha = 0.1)
 
-plt.xlabel("Iterations")
-plt.legend()
-plt.title("Performance metrics during Experimental Design")
-plt.savefig("ED_plot")
+    plt.xlabel("Iterations")
+    plt.legend()
+    plt.title("Performance metrics during Experimental Design")
+    plt.savefig("ED_plot")
 
 
 

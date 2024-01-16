aba05a55d362fb5a5fbf9d653b872c16c19c6b4d
ZenoMK <zeno.kujawa@gmail.com>
2024-01-16 16:52:17 +0000

    fun plotting

--- GP/run_ED.py
+++ GP/run_ED.py
@@ -5,6 +5,7 @@ import numpy as np
 import logging
 from utils_angles_fixed import *
 from sklearn.metrics import mean_absolute_error, mean_squared_error
+import matplotlib.pyplot as plt
 
 logging.basicConfig(level=logging.DEBUG)
 
@@ -13,19 +14,54 @@ dir = hist_data_dir = f"{os.path.dirname(os.path.dirname(os.path.realpath(__file
 test_hists = f"{os.path.dirname(os.path.dirname(os.path.realpath(__file__)))}/data_processing/new_data"
 mean_histogram, std_histogram, temps, concs, histograms = load_histograms_and_calculate_stats(test_hists)
 X_test, Y_test = prepare_2d_gp_data(histograms, temps, concs)
-obj = ExperimentalDesign(dir)
-obj.run_loop(5)
-m = obj.get_model()
-Y_pred, _ = m.predict(X_test)
-print(f"MSE: {mean_squared_error(Y_pred,Y_test)} after 5 iterations")
-obj.run_loop(1)
-m = obj.get_model()
-Y_pred, _ = m.predict(X_test)
-print(f"RMSE: {mean_squared_error(Y_test, Y_pred, squared=False)} and MAE: {mean_absolute_error(Y_test, Y_pred)}  after 15 iterations")
-obj.run_loop(1)
-m = obj.get_model()
-Y_pred, _ = m.predict(X_test)
-print(f"RMSE: {mean_squared_error(Y_test, Y_pred, squared=False)} and MAE: {mean_absolute_error(Y_test, Y_pred)}  after 25 iterations")
-X_used = obj.get_sampled_X()
-for i in range(len(X_used)):
-    print("["+str(i)+","+str(X_used[i][0])+","+str(X_used[i][1])+"],")
+
+RMSE = np.zeros((10,30))
+MAE = np.zeros((10,30))
+for i in range(10):
+    obj = ExperimentalDesign(dir)
+    m = obj.get_model()
+    Y_pred, _ = m.predict(X_test)
+    Y_test_flat = Y_test.ravel()
+    Y_pred_flat = Y_pred.ravel()
+    rmse = mean_squared_error(Y_test_flat, Y_pred_flat, squared=False)
+    mae = mean_absolute_error(Y_test, Y_pred)
+    RMSE[i, 0] = rmse
+    MAE[i, 0] = mae
+    print(RMSE)
+    print(MAE)
+    for j in range(1,30):
+        obj.run_loop(1)
+        m = obj.get_model()
+        Y_pred, _ = m.predict(X_test)
+        Y_test_flat = Y_test.ravel()
+        Y_pred_flat = Y_pred.ravel()
+        rmse = mean_squared_error(Y_test_flat, Y_pred_flat, squared=False)
+        mae = mean_absolute_error(Y_test, Y_pred)
+        RMSE[i,j] = rmse
+        MAE[i,j] = mae
+
+RMSE_means = np.mean(RMSE, axis = 1)
+MAE_means = np.mean(MAE, axis = 1)
+
+print(RMSE_means)
+print(MAE_means)
+
+RMSE_std = np.std(RMSE, axis = 1)
+MAE_std = np.std(MAE, axis = 1)
+idx = [i for i in range(10)]
+#fig, ax = plt.figure()
+plt.plot(idx, RMSE_means, color = "blue", label = "RMSE")
+plt.fill_between(x = idx, y1 = RMSE_means-RMSE_std, y2 = RMSE_means+RMSE_std, color = "blue", alpha = 0.1)
+
+plt.plot(idx, MAE_means, color = "red", label = "MAE")
+plt.fill_between(x = idx, y1 = MAE_means-MAE_std,y2 = RMSE_means+RMSE_std, color = "red", alpha = 0.1)
+
+plt.xlabel("Iterations")
+plt.legend()
+plt.title("Performance metrics during Experimental Design")
+plt.savefig("ED_plot")
+
+
+
+
+
